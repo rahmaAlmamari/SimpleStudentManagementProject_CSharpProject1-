07d7c2af7dfcf9e9063025814307b295c80720b6
rahmaAlmamari <alr816125@gmail.com>
2025-03-19 23:12:21 -0700

    declare the main arrays and variables needed in the program

--- Program.cs
+++ Program.cs
@@ -2,6 +2,22 @@
 {
     internal class Program
     {
+        /*
+         *  add the program need ....
+         *
+         *
+         *
+         *
+         *
+         *
+         */
+        // declare the array and variable needed ....
+        static double[] marks = new double[10];
+        static int[] Ages = new int[10];
+        static string[] names = new string[10];
+        static DateTime[] dates = new DateTime[10];
+        static int StudentCounter = 0;
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
